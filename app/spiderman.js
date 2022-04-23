8b8faf4891b405b201f960ed926c42138b13b89c
jhechus <jess.osm1412@gmail.com>
2022-04-23 18:40:23 -0500

    resolviendo errores y agregando todos los componentes faltantes

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,3 +1,11 @@
-class spiderman{}
+class spiderman{
+    constructor (name, age, actor, movies, studio) {
+        this.name = name
+        this.age = age
+        this.actor = actor
+        this.movies = movies
+        this.studio = studio
+    } 
+}
 
 module.exports = spiderman
--- test.js/spiderman.test.js
+++ test.js/spiderman.test.js
@@ -5,14 +5,14 @@ describe("Unit tests for spiderman class", () => {
     test('1- Create an spiderman object', () => {
 
       // escribir el codigo que queremos usar para instanciar un objeto de spiderman con esta inf.
-      const AndrewGarfield = new spiderman('Spiderman 2', 31, 'Andrew Garfield, 2, Sony')
+      const AndrewGarfield = new spiderman('Spiderman 2', 31, 'Andrew Garfield', 2, 'Sony')
       
       //esperemos que al validar el codigo funcione
 
-      expect(AndrewGarfield.name).toBe('spiderman 2')
-      expect(AndrewGarfield.age).toBe('31')
+      expect(AndrewGarfield.name).toBe('Spiderman 2')
+      expect(AndrewGarfield.age).toBe( 31 )
       expect(AndrewGarfield.actor).toBe('Andrew Garfield')
-      expect(AndrewGarfield.movies).toBe('2')
+      expect(AndrewGarfield.movies).toBe( 2 )
       expect(AndrewGarfield.studio).toBe('Sony')
     });
   })
